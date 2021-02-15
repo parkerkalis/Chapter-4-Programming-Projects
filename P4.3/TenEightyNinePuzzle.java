7cd0636f144693e3a8a75128cc3a2ae7cc004b60
loganb-kmhs <77634091+loganb-kmhs@users.noreply.github.com>
2021-02-15 09:33:53 -0600

    Create TenEightyNinePuzzle.java

---
+++ P4.3/TenEightyNinePuzzle.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/**
+ * Read a number from the user. Discard all but the last three digits.
+ * Reverse the digits, subtract the original from the reversed (discarding any
+ * minus sign), reverse the digits of the difference, and add the difference and
+ * the reversed difference. For example: Input: 371 Reversed: 173 Difference:
+ * 198 Reversed: 891 Sum: 1089
+ *
+ */
+public class TenEightyNinePuzzle.java
+{
+
+    /**
+     * Reverse the last 3 digits of number
+     *
+     * @param n the number
+     * @return the reverse of the last 3 digits of n
+     */
+    static int reverse(int n)
+    {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+      
+    }
+}
+
