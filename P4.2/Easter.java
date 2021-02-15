56341e531d50d6652de52e68b6d38b8498765b2d
loganb-kmhs <77634091+loganb-kmhs@users.noreply.github.com>
2021-02-15 09:31:09 -0600

    Create Easter.java

---
+++ P4.2/Easter.java
@@ -0,0 +1,21 @@
+public class Easter
+{
+
+
+   /**
+      Constructs the date of Easter Sunday.
+   */
+
+
+   /**
+      Gets the month of Easter Sunday
+      @return month of Easter Sunday
+   */
+
+
+   /**
+      Gets the date of Easter Sunday
+      @return date of Easter Sunday
+   */
+
+}
