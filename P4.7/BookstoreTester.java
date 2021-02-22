ca8092ca4011848524b265549aef4430fe8ed8fe
parkerkalis <78744276+parkerkalis@users.noreply.github.com>
2021-02-22 09:04:14 -0600

    Create BookstoreTester.java

---
+++ P4.7/BookstoreTester.java
@@ -0,0 +1,9 @@
+public class BookstoreTester
+{
+	public static void main(String args[])
+	{
+		bookstore order = new bookstore();
+		System.out.println(order.pricecalc(10));
+		//amount of books for order is input into pricecalc function to find price
+	}
+}
