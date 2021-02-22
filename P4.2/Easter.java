083a9cf19991b2ff2f038b8af386be8f6d51f033
parkerkalis <78744276+parkerkalis@users.noreply.github.com>
2021-02-22 09:00:47 -0600

    Update Easter.java

--- P4.2/Easter.java
+++ P4.2/Easter.java
@@ -1,21 +1,31 @@
-public class Easter
+public class easter
 {
+	int n;
+	int p;
+	public easter(int year)
+	{
 
-
-   /**
-      Constructs the date of Easter Sunday.
-   */
-
-
-   /**
-      Gets the month of Easter Sunday
-      @return month of Easter Sunday
-   */
-
-
-   /**
-      Gets the date of Easter Sunday
-      @return date of Easter Sunday
-   */
-
-}
+		int y = year;
+		int a = y % 19;
+		int b = y / 100;
+		int c = y % 100;
+		int d = b / 4;
+		int e = b % 4;
+		int g = (8 * b + 13) / 25;
+		int h = (19 * a + b - d - g + 15) % 30;
+		int j = c / 4;
+		int k = c % 4;
+		int m = (a + 11 * h) / 319;
+		int r = (2 * e + 2 * j - k - h + m + 32) % 7;
+		n = (h - m + r + 90) / 25;
+		p = (h - m + r + n + 19) % 32;
+	}
+	public int getEasterSundayMonth()
+	{
+		return n;
+	}
+	public int getEasterSundayDay()
+	{
+		return p;
+	}
+			}
