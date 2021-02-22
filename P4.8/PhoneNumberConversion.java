38817ce596538488b95348b52c6207ae2d0b03b0
parkerkalis <78744276+parkerkalis@users.noreply.github.com>
2021-02-22 09:04:42 -0600

    Update PhoneNumberConversion.java

--- P4.8/PhoneNumberConversion.java
+++ P4.8/PhoneNumberConversion.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
-
-public class PhoneNumberConversion
+public class phonenum
 {
-   public static void main(String[] args)
-   {
-
-   }
+	static String phonenum = "4155551212";
+	public static void main(String args[])
+	{
+		String sub1 = phonenum.substring(0, 3);
+		String sub2 = phonenum.substring(3, 6);
+		String sub3 = phonenum.substring(6);
+		System.out.println("(" + sub1 + ")" + sub2 + "-" + sub3);
+	}
 }
