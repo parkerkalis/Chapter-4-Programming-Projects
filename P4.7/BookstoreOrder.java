d6192fb0b9c8fadcd387535eab83279354fa8784
parkerkalis <78744276+parkerkalis@users.noreply.github.com>
2021-02-22 09:03:25 -0600

    Update BookstoreOrder.java

--- P4.7/BookstoreOrder.java
+++ P4.7/BookstoreOrder.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
-
-public class BookstoreOrder
+public class bookstore
 {
-   public static void main(String[] args)
-   {
-
-   }
+	int bookprice = 10;
+	public double pricecalc(int bookorder)
+	{
+		double numofbooks = bookorder;
+		double tax = (bookprice * numofbooks) * 0.075;
+		double shipcharge = numofbooks * 2;
+		double orderp = tax + shipcharge + (bookprice * numofbooks);
+		return orderp;
+	}
 }
